cbb0c44961ee6d35d371bfcd3187bb15573f6d75
matts <mattsheldon914@gmail.com>
2024-12-13 05:28:28 +0000

    orchestration x2

--- periphery/orchestration/orchestrator.py
+++ periphery/orchestration/orchestrator.py
@@ -1,9 +1,10 @@
 
 
 class Orchestrator:
-    def __init__(self, submodels, shard_graph):
+    def __init__(self, submodels, shard_graph, registered_nodes):
         self.submodels = submodels
         self.shard_graph = shard_graph
+        self.registered_nodes = registered_nodes
 
     def get_assignments(self):
         pass
